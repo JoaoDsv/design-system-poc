79624cea2e774eacd5a33012eeef4c91a6e322cb
Joao Da Silva Marly <joao.pro.dsvm@gmail.com>
2024-02-07 12:48:48 +0100

    Setup shadow panda

--- panda.config.ts
+++ panda.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
+  presets: ["@shadow-panda/preset"],
+
   // Whether to use css reset
   preflight: true,
+
+  // Use React
+  jsxFramework: "react",
 
   // Where to look for your css declarations
   include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
